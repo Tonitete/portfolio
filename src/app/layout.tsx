6ca81d7de8ccf4e7c72325cd46e6dd02bb32b921
Tonitete <antonio.caballero.miro@gmail.com>
2024-04-10 19:16:21 +0200

    Linted files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Head from "next/head";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Head from 'next/head'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Portfolio - Antonio Caballero",
-  description: "",
-};
+  title: 'Portfolio - Antonio Caballero',
+  description: ''
+}
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: Readonly<{
-  children: React.ReactNode;
-}>) {
+  children: React.ReactNode
+}>): JSX.Element {
   return (
-    <html lang="es">
+    <html lang='es'>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <body className={inter.className + " antialiased text-slate-200 bg-gradient-to-b from-slate-900 to-slate-700"}>{children}</body>
+      <body className={inter.className + ' antialiased text-slate-200 bg-gradient-to-b from-slate-900 to-slate-700'}>{children}</body>
     </html>
-  );
+  )
 }
